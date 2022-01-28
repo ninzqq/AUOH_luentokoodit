26aa71ad2a734288f4bd13f9cffbaf0f492c2aa6
Janne Niinisalo <janne.niinisalo@gmail.com>
2022-01-28 20:27:51 +0200

    06_websocket harjotukseen omia lisäyksiä

--- 06_websocket/app.js
+++ 06_websocket/app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8081;
 
 let connections = [];
 let number_of_connections = 0;
+let messagelist = [];
 
 const broadcast_message = (message) => {
     for(let id in connections){
@@ -20,9 +21,13 @@ io.on('connection', (socket) => {
     connections[socket.id] = socket;
     number_of_connections++;
 
-    socket.emit("server-to-client", "Hello from server");
+    let iterator = messagelist.values();
+    for(let single_message of iterator){
+        socket.emit("server-to-client", single_message);
+    }
 
     console.log('Client connected. Number of open connections: ', number_of_connections);
+
     socket.on('disconnect', (socket) => {
         delete connections[socket.id];
         number_of_connections--;
@@ -30,8 +35,10 @@ io.on('connection', (socket) => {
     });
 
     socket.on("client-to-server", (message) => {
+        messagelist.push(message);
         broadcast_message(message);
         console.log(message);
+        console.log(messagelist);
     });
 
 });
