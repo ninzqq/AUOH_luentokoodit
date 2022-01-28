98505dd3c67ce75af1a72fee056c7a94440239b4
Janne Niinisalo <janne.niinisalo@gmail.com>
2022-01-28 17:26:34 +0200

    06_websocket
    luennon aikana tehty harjotus

---
+++ 06_websocket/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const server = require("http").createServer(app);
+const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 8081;
+
+
+let connections = [];
+let number_of_connections = 0;
+
+const broadcast_message = (message) => {
+    for(let id in connections){
+        const socket = connections[id];
+        socket.emit("server-to-client", message);
+    }
+};
+
+io.on('connection', (socket) => {
+    connections[socket.id] = socket;
+    number_of_connections++;
+
+    socket.emit("server-to-client", "Hello from server");
+
+    console.log('Client connected. Number of open connections: ', number_of_connections);
+    socket.on('disconnect', (socket) => {
+        delete connections[socket.id];
+        number_of_connections--;
+        console.log('Client disconnected. Number of open connections: ', number_of_connections);
+    });
+
+    socket.on("client-to-server", (message) => {
+        broadcast_message(message);
+        console.log(message);
+    });
+
+});
+
+
+app.use(express.static("public"));
+
+
+server.listen(PORT);
+
